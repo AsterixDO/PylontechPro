de08548a504d2c5c4fe718acf2a316a327a80dbe
AsterixDO <99197068+AsterixDO@users.noreply.github.com>
2026-04-17 20:51:08 +0200

    Add files via upload

---
+++ module.php
@@ -0,0 +1,13 @@
+{
+    "id": "{9B4E9A2D-7C3F-4A0A-9A9A-112233445566}",
+    "name": "PylontechPro",
+    "type": 3,
+    "vendor": "Custom",
+    "aliases": [
+        "Pylontech Battery RS485"
+    ],
+    "parentRequirements": [],
+    "childRequirements": [],
+    "implemented": [],
+    "prefix": "PYLON"
+}
